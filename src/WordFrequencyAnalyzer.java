fd33cbf85dd9477b95011e99235f1efa7679018c
James <jamescelona@gmail.com>
2012-11-15 15:28:35 -0500

    add classes for assignments

---
+++ src/WordFrequencyAnalyzer.java
@@ -0,0 +1,25 @@
+/**
+ * This class will be keeping track of the frequency of word occurrences
+ * within a text file (a GUI will allow users to select the file).
+ * 
+ * Notable Classes that this class interacts with:
+ * WordCounter.java
+ * ReportGenerator.java
+ * WordFrequencyCollection.java
+ * 
+ * CRC Card:
+ *| ------------------------------------|
+ *|Word Frequency Collection   	        |
+ *|-------------------------------------|
+ *|Ability to add a word to collection  |
+ *|given a word, report frequency	    |
+ *|iterator			   				    |
+ *|-------------------------------------|
+ * 
+ * 
+ * @author James Celona
+ *@author Joe Young
+ *
+ */
+public class WordFrequencyAnalyzer {
+}
